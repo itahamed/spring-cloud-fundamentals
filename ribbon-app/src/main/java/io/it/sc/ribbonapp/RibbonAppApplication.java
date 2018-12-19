3f581bb91177d26399d6cd53d8d5cc9b5281847f
Thahir Ahamed <itahamed@gmail.com>
2018-12-19 19:34:01 +0800

    ribbon app

---
+++ ribbon-app/src/main/java/io/it/sc/ribbonapp/RibbonAppApplication.java
@@ -0,0 +1,14 @@
+package io.it.sc.ribbonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonAppApplication.class, args);
+	}
+
+}
+
